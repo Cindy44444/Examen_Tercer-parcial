9a830619e140ab58520e6617cbe893285938c385
Lab2-13 <Lab2-13@LAB2-13>
2025-04-08 11:14:06 -0600

    ahhhhh

---
+++ Acciones.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_Tercer_parcial
+{
+    internal class Acciones
+    {
+        List<herencia> listaHerencia = new List<herencia>
+        {
+            new herencia ("clave", "name 1","edad","1","2","3"),
+            new herencia ("clave", "name 2","edad","1","2","3"),
+            new herencia ("clave", "name 3","edad","1","2","3"),
+            new herencia ("clave", "name 4","edad","1","2","3"),
+            new herencia ("clave", "name 5","edad","1","2","3"),
+        };
+        herencia a = new herencia();
+        public List<herencia> MostrarHerencia()
+        {
+            return listaHerencia;
+        }
+    }
+}
