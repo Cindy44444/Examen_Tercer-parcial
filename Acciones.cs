1434480f505317b52d30d69136b6fa8bcce33904
Lab2-06 <Lab2-06@LAB2-06>
2025-04-08 18:01:49 -0600

    FTYR N

--- Acciones.cs
+++ Acciones.cs
@@ -10,11 +10,7 @@ namespace Examen_Tercer_parcial
     {
         List<herencia> listaHerencia = new List<herencia>
         {
-            new herencia (3467, "name 1",01/04/2022,"si","azul","nose"),
-            new herencia (5987, "name 2",01/06/2022,"si","verde", "vivo"),
-            new herencia (4017, "name 3",09/06/2022,"no", "morado", "muero"),
-            new herencia (4028, "name 4",10/07/2022,"si","cafe", "vivo"),
-            new herencia (1892, "name 5",25/07/2022,"no","amarillo", "muerto"),
+            new herencia (3467, "name 1",DateTime.Today,,"azul","nose"),
         };
         herencia a = new herencia();
         public List<herencia> MostrarHerencia()
